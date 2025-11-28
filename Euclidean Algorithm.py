cc86e6a784dd10a7f8dfb2b1845e03ebc8932b81
xuzikang666 <2322816247@qq.com>
2025-11-29 01:42:34 +0800

    second work
    add some comments

--- Euclidean Algorithm.py
+++ Euclidean Algorithm.py
@@ -5,5 +5,6 @@ Created on Fri Nov 28 08:36:17 2025
 
 @author: xuzikang
 """
+#import the code that i wrote
 from mymath import EA_math
 print('the GCD of a and b is:`' ,EA_math.main(s))
--- mymath.py
+++ mymath.py
@@ -5,20 +5,27 @@ Created on Fri Nov 28 08:31:52 2025
 
 @author: xuzikang
 """
+#create a class
 class EA_math():
-    
+#create a function that put a and b into it
     def __init__(self, a, b):
         self.a = a
         self.b = b
+#create a function that include the method of Euclidean Algorithm
     def gcd(self):
         a = self.a
         b = self.b
+#when b is not 0 and a more than or equal b and the code can run and it is a loop that continues until b equals 0 or a is less than b
         while b != 0 and a >= b:
+#calculate the remainder of a and b
             remainder = a % b
+#the length of a equal the last b
             a = b
+#the length of b equal the remainder that be calculated in the last time
             b = remainder
         return a
     def main():
+#input the value of a and b
         algo = EA_math(a=9, b=6)
         result = algo.gcd()
         return result
