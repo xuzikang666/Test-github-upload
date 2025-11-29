232ed5ea0d5583044ca6c7c311924f786cf5eedd
xuzikang666 <2322816247@qq.com>
2025-11-29 23:57:46 +0800

    refactor work
    refactor it and add some command

--- Euclidean Algorithm.py
+++ Euclidean Algorithm.py
@@ -5,6 +5,27 @@ Created on Fri Nov 28 08:36:17 2025
 
 @author: xuzikang
 """
-#import the code that i wrote
+import my ode
 from mymath import EA_math
-print('the GCD of a and b is:`' ,EA_math.main())
+# create a function
+def is_valid_integer(value):
+    return value.isdigit() and int(value) > 0
+
+
+if __name__ == "__main__":
+    # let user to write numbers
+    A_input = input("Please enter the first positive integer: ")
+    B_input = input("Please enter the second positive integer: ")
+
+    # check numbers
+    if not (is_valid_integer(A_input) and is_valid_integer(B_input)):
+        print("ERROR: Both values must be positive integers!")
+    else:
+        a = int(A_input)
+        b = int(B_input)
+
+        # create EA_math object
+        algo = EA_math(a, b)
+        result = algo.gcd()
+
+        print(f"The GCD of {a} and {b} is: {result}")
--- mymath.py
+++ mymath.py
@@ -5,27 +5,21 @@ Created on Fri Nov 28 08:31:52 2025
 
 @author: xuzikang
 """
-#create a class
-class EA_math():
-#create a function that put a and b into it
+class EA_math:
+    # create a and b
     def __init__(self, a, b):
         self.a = a
         self.b = b
-#create a function that include the method of Euclidean Algorithm
+
+    # the skill of Euclidean Algorithm and define them
     def gcd(self):
         a = self.a
         b = self.b
-#when b is not 0 and a more than or equal b and the code can run and it is a loop that continues until b equals 0 or a is less than b
-        while b != 0 and a >= b:
-#calculate the remainder of a and b
+
+       
+        while b != 0:
             remainder = a % b
-#the length of a equal the last b
             a = b
-#the length of b equal the remainder that be calculated in the last time
             b = remainder
+
         return a
-    def main():
-#input the value of a and b
-        algo = EA_math(a=9, b=6)
-        result = algo.gcd()
-        return result
